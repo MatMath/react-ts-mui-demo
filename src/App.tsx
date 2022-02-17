b6f1f77ed08b142ea2f5ece7cf02c035021e9b73
Mathieu <mathieu.k.legault@cyndx.com>
2022-02-17 09:59:33 -0500

    feat: adding mui and example css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import StyledEngineProvider from "@mui/material/StyledEngineProvider";
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
 
-function App() {
+import { Content } from './Content';
+
+import './PlainCss.css';
+interface FooterComponentProps {
+  linkName: string
+}
+const FooterComponent = ({ linkName }: FooterComponentProps): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Typography className='footerItem' variant="body2" color="text.secondary" align="center">
+      This is a footer
+      <Link className='footerItem' color="inherit" href="https://mui.com/">
+        {linkName}
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+      <StyledEngineProvider injectFirst>
+        <CssBaseline />
+        <Container maxWidth="sm">
+          <Box sx={{ my: 4 }}>
+            <Typography variant="h4" component="h1" gutterBottom>
+              Create React App example with TypeScript
+            </Typography>
+            <Content />
+            <FooterComponent linkName={'Link to your website'} />
+          </Box>
+        </Container>
+      </StyledEngineProvider>
+  );
+}
